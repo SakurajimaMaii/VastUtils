4c5a203242058e49d7d0055b6eda8d63411cada9
entropy <guihy2019@foxmail.com>
2021-11-01 15:17:18 +0800

    Add Rules of International Mobile Phone Numbers link

--- Tools/src/main/java/com/gcode/tools/utils/IntentUtils.kt
+++ Tools/src/main/java/com/gcode/tools/utils/IntentUtils.kt
@@ -12,6 +12,12 @@ import java.util.*
 object IntentUtils {
     /**
      * Dial phone number
+     *
+     * **This method does not verify the phone number.**
+     * If you want to know if your mobile phone number
+     * meets the rules,you can click this link
+     * [Rules of International Mobile Phone Numbers](https://support.huaweicloud.com/intl/en-us/productdesc-msgsms/phone_numbers.html)
+     *
      * @param phoneNumber Phone number you want to call.
      */
     fun dialPhoneNumber(phoneNumber: String) =

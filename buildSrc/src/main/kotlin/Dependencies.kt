fff9ccf27403495e559ac0aa3ba3d470a38dea5e
Vast Gui <1550651926@qq.com>
2022-07-14 23:04:50 +0800

    使用buildSrc对第三方依赖库进行了统一管理

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2022 VastGui
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+// Author: Vast Gui
+// Email: guihy2019@gmail.com
+// Date: 2022/7/14
+// Description: 
+// Documentation:
+
+object Dependencies {
+
+    private const val activity_compose_version = "1.4.0"
+    private const val activity_version = "1.5.0"
+    private const val annotation_version = "1.4.0"
+    private const val appcompat_version = "1.4.2"
+    private const val arch_core = "2.1.0"
+    private const val camerax_version = "1.0.0-alpha06"
+    private const val compose_foundation = "1.2.0-rc03"
+    private const val compose_version = "1.2.0-beta01"
+    private const val constraintlayout_version = "2.1.4"
+    private const val core_ktx_version = "1.8.0"
+    private const val dokka_version = "1.6.10"
+    private const val espresso_core_version = "3.4.0"
+    private const val ext_junit_version = "1.1.3"
+    private const val fragment_version = "1.5.0"
+    private const val gradle_version = "7.4.0-alpha08"
+    private const val junit_version = "4.13.2"
+    private const val kotlin_reflect_version = "1.6.21"
+    private const val kotlin_version = "1.6.21"
+    private const val lifecycle_version = "2.5.0-rc01"
+    private const val material3_version = "1.0.0-alpha11"
+    private const val material_version = "1.6.1"
+    private const val okhttp3_version = "5.0.0-alpha.6"
+    private const val progress_manager_version = "v1.5.0"
+    private const val recyclerview_version = "1.2.1"
+    private const val splashscreen_version = "1.0.0-rc01"
+    private const val start_up_version = "1.1.1"
+    private const val test_monitor = "1.5.0"
+
+    // androidX
+    const val version_arch_core_runtime = "androidx.arch.core:core-runtime:$arch_core"
+    const val version_activity = "androidx.activity:activity:$activity_version"
+    const val version_activity_ktx = "androidx.activity:activity-ktx:$activity_version"
+    const val version_annotation = "androidx.annotation:annotation:$annotation_version"
+    const val version_appcompat = "androidx.appcompat:appcompat:$appcompat_version"
+    const val version_constraintlayout = "androidx.constraintlayout:constraintlayout:$constraintlayout_version"
+    const val version_core = "androidx.core:core:$core_ktx_version"
+    const val version_core_ktx = "androidx.core:core-ktx:$core_ktx_version"
+    const val version_core_splashscreen = "androidx.core:core-splashscreen:$splashscreen_version"
+    const val version_espresso_core = "androidx.test.espresso:espresso-core:$espresso_core_version"
+    const val version_ext_junit = "androidx.test.ext:junit:$ext_junit_version"
+    const val version_fragment = "androidx.fragment:fragment:$fragment_version"
+    const val version_fragment_ktx = "androidx.fragment:fragment-ktx:$fragment_version"
+    const val version_lifecycle_livedata = "androidx.lifecycle:lifecycle-livedata:$lifecycle_version"
+    const val version_lifecycle_livedata_ktx = "androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version"
+    const val version_lifecycle_livedata_core = "androidx.lifecycle:lifecycle-livedata-core:$lifecycle_version"
+    const val version_lifecycle_livedata_core_ktx = "androidx.lifecycle:lifecycle-livedata-core-ktx:$lifecycle_version"
+    const val version_lifecycle_runtime = "androidx.lifecycle:lifecycle-runtime:$lifecycle_version"
+    const val version_lifecycle_runtime_ktx = "androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version"
+    const val version_lifecycle_viewmodel = "androidx.lifecycle:lifecycle-viewmodel:$lifecycle_version"
+    const val version_lifecycle_viewmodel_ktx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version"
+    const val version_recyclerview = "androidx.recyclerview:recyclerview:$recyclerview_version"
+    const val version_start_up_runtime_version = "androidx.startup:startup-runtime:$start_up_version"
+    const val version_test_monitor = "androidx.test:monitor:$test_monitor"
+
+    const val version_junit = "junit:junit:$junit_version"
+
+    const val version_kotlin_reflect = "org.jetbrains.kotlin:kotlin-reflect:$kotlin_reflect_version"
+
+    // other
+    const val version_progressmanager = "com.github.JessYanCoding:ProgressManager:$progress_manager_version"
+    const val version_okhttp3 = "com.squareup.okhttp3:okhttp:$okhttp3_version"
+
+    // Material UI
+    const val version_material = "com.google.android.material:material:$material_version"
+    const val version_material3 = "androidx.compose.material3:material3:$material3_version"
+
+    // Compose
+    const val version_compose_activity = "androidx.activity:activity-compose:$activity_compose_version"
+    const val version_compose_foundation = "androidx.compose.foundation:foundation:$compose_foundation"
+    const val version_compose_foundation_layout = "androidx.compose.foundation:foundation-layout:$compose_foundation"
+    const val version_compose_ui = "androidx.compose.ui:ui:$compose_version"
+    const val version_compose_ui_test_junit4 = "androidx.compose.ui:ui-test-junit4:$compose_version"
+    const val version_compose_ui_test_manifest = "androidx.compose.ui:ui-test-manifest:$compose_version"
+    const val version_compose_ui_tooling = "androidx.compose.ui:ui-tooling:$compose_version"
+    const val version_compose_ui_tooling_preview = "androidx.compose.ui:ui-tooling-preview:$compose_version"
+}
